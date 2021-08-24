42751248649a943fa9293da681f1ccd3da256f83
Igor V. Teixeira <igorvitort@gmail.com>
2021-08-23 22:14:13 -0300

    Adição da tela de ajuda

---
+++ pages/ajuda.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+def app():
+    st.write("# Ajuda")
+
+    st.write("## Caso possua alguma dúvida a respeito da utilização da plataforma, você pode assistir ao nosso vídeo introdutório:")
+
+    st.video("https://www.youtube.com/watch?v=3ipbYO4zJHo")
--- valeria_1.py
+++ valeria_1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from pages import inicio, diagnostico, casos, sobre
+from pages import inicio, diagnostico, casos, ajuda, sobre
 from pages.page_manager import PageManager
 
 app = PageManager()
@@ -10,6 +10,7 @@ st.set_page_config(page_title="VALERIA", page_icon="images/val_temp.png")
 app.add_page("Início", inicio.app)
 app.add_page("Diagnóstico", diagnostico.app)
 app.add_page("Casos", casos.app)
+app.add_page("Ajuda", ajuda.app)
 app.add_page("Sobre", sobre.app)
 
 app.run()
