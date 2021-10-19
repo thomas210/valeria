09017bef8cd7df85a95ac8e90c47f89b865b357f
thomas210 <thomastabosa10@gmail.com>
2021-10-19 03:25:40 -0300

    :rocket: explainer done

--- models/patient.py
+++ models/patient.py
@@ -12,14 +12,14 @@ class Patient:
         """Construtor da classe, inicialização das labels e definição do caminho para o modelo de ML.
         """
 
-        #Saídas formatadas do modelo para visualização no front
+        #Saídas formatadas do modelo para visualização no front.
         self.outputs = {
             "CHIKUNGUNYA": "Chikungunya",
             "DENGUE": "Dengue",
             "OUTRAS_DOENCAS": "Inconclusivo"
         }
 
-        # Inputs categóricos(binários) do modelo
+        # Inputs categóricos(binários) do modelo.
         self.categorical_labels = {
             "FEBRE": "Febre", "MIALGIA": "Mialgia", "CEFALEIA": "Cefaleia", "EXANTEMA": "Exantema", "NAUSEA": "Náusea", "DOR_COSTAS": "Dor nas costas",
             "CONJUNTVIT": "Conjuntivite", "ARTRITE": "Artrite", "ARTRALGIA": "Artralgia", "PETEQUIA_N": "Petéquias", "DOR_RETRO": "Dor Retroorbital",
@@ -33,15 +33,15 @@ class Patient:
 
         self.labels = dict(self.categorical_labels, **self.numerical_labels)
 
-        # caminho onde está localizado o modelo de ML
+        # caminho onde está localizado o modelo de ML.
         self.path_model_ml = "ml\\gradient_model.pkl"
 
     def diagnosis (self):
-        """Realiza o dianóstico do paciente, utilizando os dados dos atributos para realizar a classificação pelo modelo de ML
+        """Realiza o dianóstico do paciente, utilizando os dados dos atributos para realizar a classificação pelo modelo de ML.
 
         Returns:
-            string: o resultado da classificação do modelo formatado para visualização 
-            pandas.Dataframe object: dataframe contendo as probablidades de cada saída do modelo com o padrão [doença | probabilidade]
+            string: o resultado da classificação do modelo formatado para visualização;
+            pandas.Dataframe object: dataframe contendo as probablidades de cada saída do modelo com o padrão [doença | probabilidade];
         """
         
         with open(self.path_model_ml, "rb") as f:
@@ -53,17 +53,18 @@ class Patient:
             prob_df = pd.DataFrame(
                 ['{:.2%}'.format(i) for i in prob[0]],
                 index=self.outputs.values(),
-                columns=["Porcentagem"] # Necessário em português para visualização no front
+                columns=["Porcentagem"] # Necessário em português para visualização no front.
             )
             return self.outputs[self.classification], prob_df
 
     def explainer(self):
-        """Utiliza o LIME para explicação do predição do dianóstico
+        """Utiliza o LIME para explicação do predição do dianóstico.
 
         Returns:
-            pandas.Dataframe object: Dataframe contendo o valor de importância para cada atributo, seguinto o padrão [value] com cada atributo como index
+            pandas.Dataframe object: Dataframe contendo o valor de importância para cada atributo, seguinto o padrão [value] com cada atributo como index.
         """
 
+        # O Explainer necessita da base de dados de treinamento para conseguir calcular os pesos, para se ter um certo nível de segurança, eu coloquei o caminho da base de dados no secrets do streamlit para que funciona como um ".env" no nosso projeto.
         path_database = st.secrets["path_database"]
         database = pd.read_csv(path_database, sep=';', usecols=self.labels.keys())
 
@@ -91,95 +92,117 @@ class Patient:
 
         exp_dict = dict(sorted(exp.as_list(label=pos_label)))
 
+        # TODO: ver com o pessoal se é melhor deixar "Peso" ou "Influência" mesmo.
+        # TODO: SE POSSÍVEL, analisar depois uma foram simples para pegar o valor de intervalo do atributo "Dias", no explainer é usado um intervalo, tipo "DIAS >= 2", na conversão essa informação se perde, e ela pode ser bem interessante.
         exp_df = pd.DataFrame(
             exp_dict.values(),
-            columns=["Influência"],
+            columns=["Peso"],
             # index=[value for key, value in sorted(self.labels.items())]
             index=dict(sorted(self.labels.items())).values()
-        )
-        # .sort_values(by=["Influência"], ascending=False)
+        ).sort_values(by=["Peso"], ascending=False)
 
-        exp_pos = exp_df[exp_df["Influência"] > 0]
-        exp_neg = exp_df[exp_df["Influência"] < 0]
+        exp_pos = exp_df[exp_df["Peso"] > 0]
+        exp_neg = exp_df[exp_df["Peso"] < 0]
 
         # return exp_df
-        return exp_df, exp_pos, exp_neg
+        return exp_pos, exp_neg
 
-    # Setters
+    # TODO: Ver com o pessoal se isso aqui é realmente necessário.
+    def eraseData(self):
+        """Método para limpeza dos dados do paciente, com isso todas as informações da ficha clínica do paciente são apagadas do sistema
+        """
 
-    # Input Febre
+        self.fever = None
+        self.myalgia = None
+        self.headache = None
+        self.rash = None
+        self.nausea = None
+        self.backPain = None
+        self.conjunctivitis = None
+        self.arthritis = None
+        self.arthralgia = None
+        self.petechia = None
+        self.eyaPain = None
+        self.diabetes = None
+        self.hypertension = None
+        self.days = None
+
+
+    # Setters.
+
+    # Input Febre.
     def setFever (self, value):
         self.fever = value
 
-    # Input Mialgia
+    # Input Mialgia.
     def setMyalgia (self, value):
         self.myalgia = value
 
-    # Input Cefaleia
+    # Input Cefaleia.
     def setHeadache (self, value):
         self.headache = value
 
-    # Input Exantema
+    # Input Exantema.
     def setRash (self, value):
         self.rash = value
 
-    # Input Náusea
+    # Input Náusea.
     def setNausea (self, value):
         self.nausea = value
 
-    # Input Dor Costas
+    # Input Dor Costas.
     def setBackPain (self, value):
         self.backPain = value
 
-    # Input Conjuntvit
+    # Input Conjuntvit.
     def setConjunctivitis (self, value):
         self.conjunctivitis = value
 
-    # Input Artrite
+    # Input Artrite.
     def setArthritis (self, value):
         self.arthritis = value
 
-    # Input Artralgia
+    # Input Artralgia.
     def setArthralgia (self, value):
         self.arthralgia = value
 
-    # Input Petéquia
+    # Input Petéquia.
     def setPetechia (self, value):
         self.petechia = value
 
-    # Input Dor Retroorbital
+    # Input Dor Retroorbital.
     def setEyePain (self, value):
         self.eyaPain = value
 
-    # Input Diabetes
+    # Input Diabetes.
     def setDiabetes (self, value):
         self.diabetes = value
 
-    # Input Hipertensão
+    # Input Hipertensão.
     def setHypertension (self, value):
         self.hypertension = value
 
-    # Input Dias
+    # Input Dias.
     def setDays (self, value):
         self.days = value
 
 
     
-    # Getters
+    # Getters.
     
     def getLabels(self):
-        """Método get para obter todas as labels utlizada no modelo de ML
+        """Método get para obter todas as labels utlizada no modelo de ML.
 
         Returns:
-            list: array com todas as labels do modelo de ML
+            list: array com todas as labels do modelo de ML.
         """
         return self.labels.keys()
 
     def getRecord(self):
-        """Get para retornar a ficha médica do paciente, com todas as informações dos atributos. IMPORTANTE: É NECESSÁRIO ESTAR NA MESMA ORDEM EM QUE O MODELO DE ML FOI TREINADO
+        """Get para retornar a ficha médica do paciente, com todas as informações dos atributos. IMPORTANTE: É NECESSÁRIO ESTAR NA MESMA ORDEM EM QUE O MODELO DE ML FOI TREINADO.
 
         Returns:
-            list: array dos os valores dos atributos do paciente
+            list: array dos os valores dos atributos do paciente.
         """
         
         return [[
--- pages/diagnosis.py
+++ pages/diagnosis.py
@@ -1,70 +1,86 @@
 import streamlit as st
 from models.patient import Patient
+# from pages import help
 
-def highlight_values(val): 
-    color = 'green' if val > 0 else 'red' 
-    return 'color: %s' % color
+# def highlight_values(val): 
+#     color = 'green' if val > 0 else 'red' 
+#     return 'color: %s' % color
 
 def app():
 
-    st.write("## Preencha os campos com as informações para realizar o diagnóstico:")
+    st.write("## Preencha os campos com as informações do paciente para realizar o diagnóstico:")
 
     with st.form("diagnostico_form"):
 
         patient = Patient()
 
-        patient.setDays(st.number_input("Quantos dias está sentindo os sintomas?", min_value=0, format="%d"))
+        patient.setDays(st.number_input("Quantos dias o paciente está sentindo os sintomas?", min_value=0, format="%d"))
 
-        st.write("Informe os sintomas:")
+        st.write("Informe os sintomas do paciente:")
 
-        patient.setFever(st.checkbox("Febre"))
+        col_symptoms_1, col_symptoms_2 = st.columns(2)
 
-        patient.setMyalgia(st.checkbox("Mialgia", help="Dor muscular"))
+        patient.setFever(col_symptoms_1.checkbox("Febre"))
 
-        patient.setHeadache(st.checkbox("Cefaleia", help="Dor de cabeça"))
+        patient.setMyalgia(col_symptoms_1.checkbox("Mialgia", help="Dor muscular"))
 
-        patient.setRash(st.checkbox("Exantema", help="Manchas vermelhas em um região"))
+        patient.setHeadache(col_symptoms_1.checkbox("Cefaleia", help="Dor de cabeça"))
 
-        patient.setNausea(st.checkbox("Náusea"))
+        patient.setRash(col_symptoms_1.checkbox("Exantema", help="Manchas vermelhas em um região"))
 
-        patient.setBackPain(st.checkbox("Dor nas costas"))
+        patient.setNausea(col_symptoms_1.checkbox("Náusea"))
 
-        patient.setConjunctivitis(st.checkbox("Conjutivite"))
+        patient.setBackPain(col_symptoms_1.checkbox("Dor nas costas"))
 
-        patient.setArthritis(st.checkbox("Artrite", help="Inflamação das articulações"))
+        patient.setConjunctivitis(col_symptoms_2.checkbox("Conjutivite"))
 
-        patient.setArthralgia(st.checkbox("Artralgia", help="Dor nas articulações"))
+        patient.setArthritis(col_symptoms_2.checkbox("Artrite", help="Inflamação das articulações"))
 
-        patient.setPetechia(st.checkbox("Petéquias", help="Pequenas manchas vermelhas ou marrom que surgem geralmente aglomeradas, mais frequentemente nos braços, pernas ou barriga"))
+        patient.setArthralgia(col_symptoms_2.checkbox("Artralgia", help="Dor nas articulações"))
 
-        patient.setEyePain(st.checkbox("Dor Retroorbital", help="Dor ao redor dos olhos"))
+        patient.setPetechia(col_symptoms_2.checkbox("Petéquias", help="Pequenas manchas vermelhas ou marrom que surgem geralmente aglomeradas, mais frequentemente nos braços, pernas ou barriga"))
 
-        st.write("Informe as condições prévias:")
+        patient.setEyePain(col_symptoms_2.checkbox("Dor Retroorbital", help="Dor ao redor dos olhos"))
 
-        patient.setDiabetes(st.checkbox("Diabetes", help=""))
+        st.write("Informe as condições prévias do paciente:")
 
-        patient.setHypertension(st.checkbox("Hipertensão", help=""))
+        col_comorbidities_1, col_comorbidities_2 = st.columns(2)
+
+        patient.setDiabetes(col_comorbidities_1.checkbox("Diabetes", help=""))
+
+        patient.setHypertension(col_comorbidities_2.checkbox("Hipertensão", help=""))
 
         if (st.form_submit_button("Realizar Diagnóstico")):
 
             with st.spinner('Realizando diagnóstico...'):
                 result, probability_df = patient.diagnosis()
-                e0, e1, e2 = patient.explainer()
+                exp_pos, exp_neg = patient.explainer()
+                patient.eraseData()
 
             st.write(f'## O resultado do diagnóstico foi **{result}**')
 
-            st.write("Abaixo é possível observar o resultado detalhado do diagnóstico, informado a probabilidade de cada doença:")
+            st.write("### Resultado detalhado")
+
+            st.write(f"Abaixo é possível observar o resultado detalhado do diagnóstico. Caso haja alguma dúvida sobre como esses valores foram gerados, você pode consultar a tela de Ajuda no canto esquerdo.")
+
+            st.write("#### Probabilidade de cada doença")
             st.dataframe(probability_df)
 
-            st.write("### Resultado detalhado")
-            st.write(f"Os valores abaixo buscam explicar melhor como o sistema de _machine learning_ chegou no resultado {result}. Para cada atributo foi aplicado um valor que representa o quão influente o mesmo foi para esta classificação, quanto maior o valor, mais forte foi a sua influência nesta classificação. Valores positivos estão coloridos em verde e indicam influência a favor da classificação {result}, enquanto que valores negativos estão coloridos em vermelho e indicam uma influência contrária a classificação {result}.")
+            # Eu tirei essa explicação daqui pq coloquei na tela de ajuda, com a ideia de ficar mais clean essa tela
 
-            st.write("Abaixo é possível analisar quais atributos influenciaram positivamente")
-            st.write
-            # st.bar_chart(explainer)
-            st.dataframe(e0.style.applymap(highlight_values))
-            st.write(e1.style.background_gradient(cmap='Greens'))
-            st.write(e2.style.background_gradient(cmap='Reds'))
+            # st.write(f"Os valores abaixo buscam explicar melhor como o sistema de _machine learning_ chegou no resultado {result}. Para cada atributo foi aplicado um valor que representa o quão influente o mesmo foi para esta classificação, quanto maior o valor, mais forte foi a sua influência nesta classificação. Valores positivos estão coloridos em verde e indicam influência a favor da classificação {result}, enquanto que valores negativos estão coloridos em vermelho e indicam uma influência contrária a classificação {result}. Para mais informações sobre como esse número é gerado, você pode consultar a tela de {help.getName()} no canto esquerdo.")
+
+            st.write("#### Peso de cada atributo para este diagnóstico provável")
+
+            # st.dataframe(e0.style.applymap(highlight_values))
+
+            col_pos, col_neg = st.columns(2)
+
+            col_pos.write(f"Atributos que contribuíram para o resutlado {result}")
+            col_pos.dataframe((exp_pos.style.background_gradient(cmap='Greens')))
+
+            col_neg.write(f"Atributos que não contribuíram para o resultado {result}")
+            col_neg.dataframe((exp_neg.style.background_gradient(cmap='Reds')))
 
             st.write("---")
             st.warning("**AVISO IMPORTANTE: este resultado é proveniente de um modelo de _machine learning_, não é definitivo. Analise também a situação epidemiológica da sua região.**")
